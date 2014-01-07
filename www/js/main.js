436fd7548d1ac916a263d4cb7d11c2e5ceca5a83
Arian Tay <ariantay@gmail.com>
2014-01-07 08:23:47 -0800

    Popup in tour_home is now bilingual
    fixed by dynamically inserting popup text based on language

--- www/js/main.js
+++ www/js/main.js
@@ -91,25 +91,17 @@ var app = {
         if (cur_page == 1 && lock == 0){
             lock = 1;
 			console.log("calling on success");			
-			//var el = $(document.createElement('div'));
-			//$(el).attr('id', 'temp');
-		for (var i=0; i<app.numStatues; i++) {
-			var statue = app.store.statues[i];
-			var distance = app.getDistanceFromLatLonInFeet(position.coords.latitude,position.coords.longitude,statue.lat,statue.lon);
-			var htmlString = 'id_' + statue.id + ' is ' + Math.floor(distance) + ' feet away<br/>';
-			//el.append(htmlString);
-			//alert("Statue: " + statue.id + ", Distance " + distance);
-            if(distance <= statue.distance && cur_statue != statue.id){
-                app.routeTo(statue.id);
-                return;
-            }
+			for (var i=0; i<app.numStatues; i++) {
+				var statue = app.store.statues[i];
+				var distance = app.getDistanceFromLatLonInFeet(position.coords.latitude,position.coords.longitude,statue.lat,statue.lon);
+				var htmlString = 'id_' + statue.id + ' is ' + Math.floor(distance) + ' feet away<br/>';
+				if(distance <= statue.distance && cur_statue != statue.id){
+					app.routeTo(statue.id);
+					return;
+				}
+			}
+			lock = 0;
 		}
-		//alert("End of onSuccess");
-		//var el2 = $("<div/>");
-		//$(el2).append(el);
-        //$('#statue_text').html(el);
-            lock = 0;
-        }
 	},
 	onError: function (error) {
 		alert('code: '    + error.code    + '\n' +
@@ -129,7 +121,7 @@ var app = {
 		return d;
 	},  
 	deg2rad: function (deg) {
-		return deg * (Math.PI/180)
+		return deg * (Math.PI/180);
 	},    
     initialize: function() {
 		//this.statueID = 0;	// **not needed, explicitly call destination **
@@ -171,11 +163,6 @@ $(document).on("pagebeforeshow", "#homepage", function () {
 		$('#home_text').html("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;International Spirit of Riverside</br></br>" +
 		"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Riverside has long maintained a spirit of Internationalism and recognition of its multicultural history.  Going back to Frank Miller, the founder of the Mission Inn, Riverside has hosted dignitaries from countries all over the world and provided leadership on an International scale.  Riverside’s multiculturism has existed for nearly 150 years with large segments of various cultures within its population going back to the 1870s.  For example the Mission Inn hosted Japanese, Russian and European dignitaries, national and important state politicians and celebrities, such as several US presidents, Prince Kaya of Japan, Prince Gustav of Sweden, Booker T. Washington, John Muir, and Amelia Earhart.  The World Affairs Council was started in Riverside at the Mission Inn (and was once attended by John F. Kennedy here) and other international peace and social conferences have been hosted here.</br></br>" + 
 		"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Riverside was the first American city to take part in the International Sister City program initiated after World War II. That tradition continues today with a robust and global Sister City program including cities in the countries of Japan, Mexico, Korea, China, India, Ghana and Germany.  The Statues of Main Street Riverside embody this spirit of internationalism with recognition of various significant civil rights and historical leaders, some with international or national significance, and others of prominent local importance.");
-		/*
-		$('#paragraph1').html("International Spirit of Riverside");
-		$('#paragraph2').html("Riverside has long maintained a spirit of Internationalism and recognition of its multicultural history.  Going back to Frank Miller, the founder of the Mission Inn, Riverside has hosted dignitaries from countries all over the world and provided leadership on an International scale.  Riverside’s multiculturism has existed for nearly 150 years with large segments of various cultures within its population going back to the 1870s.  For example the Mission Inn hosted Japanese, Russian and European dignitaries, national and important state politicians and celebrities, such as several US presidents, Prince Kaya of Japan, Prince Gustav of Sweden, Booker T. Washington, John Muir, and Amelia Earhart.  The World Affairs Council was started in Riverside at the Mission Inn (and was once attended by John F. Kennedy here) and other international peace and social conferences have been hosted here.");
-		$('#paragraph3').html("Riverside was the first American city to take part in the International Sister City program initiated after World War II. That tradition continues today with a robust and global Sister City program including cities in the countries of Japan, Mexico, Korea, China, India, Ghana and Germany.  The Statues of Main Street Riverside embody this spirit of internationalism with recognition of various significant civil rights and historical leaders, some with international or national significance, and others of prominent local importance.");
-		*/
 	}else{
 		$('#header h3').html("Ciudad de Riverside Guía de Turismo");
 		$('#map_link span.ui-btn-text').html("Comensar");
@@ -183,12 +170,8 @@ $(document).on("pagebeforeshow", "#homepage", function () {
 		$('#settings_link span.ui-btn-text').html("Ajustes");
 		$('#home_text').html("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;El Orgullo International de Riverside</br></br>" +
 		"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Riverside es una ciudad que reconoce su historia multicultural y esta orgullosa de sus relaciones internacionales que mantiene hasta ahora. Todo empezó con el dueño del Mission Inn el señor Frank Miller que invitaba a dignatarios del rededor del mundo a que se hospedaran en este lugar. La riqueza de tantas culturas ha existido por más de 150 años comenzando desde 1870.</br></br>" + 
-		"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Riverside was the first American city to take part in the International Sister City program initiated after World War II. That tradition continues today with a robust and global Sister City program including cities in the countries of Japan, Mexico, Korea, China, India, Ghana and Germany.  The Statues of Main Street Riverside embody this spirit of internationalism with recognition of various significant civil rights and historical leaders, some with international or national significance, and others of prominent local importance.");
-		/*
-		$('#paragraph1').html("Media t'ida es la candcla, pan y vino, la otra media");
-		$('#paragraph2').html("Caminante, son tus huellas el camino, y nada más; caminante, no hay camino, se hace camino al andar. Al andar se hace camino, y al volver la vista atrás se ve la senda que nunca se ha de volver a pisar.");
-		$('#paragraph3').html("Media t'ida es la candcla, pan y vino, la otra media. Gracias a dios. Hasta luego. Caminante, son tus huellas el camino, y nada más; caminante, no hay camino, se hace camino al andar. Al andar se hace camino, y al volver la vista atrás se ve la senda que nunca se ha de volver a pisar. Caminante, no hay camino, sino estelas en la mar. Media t'ida es la candcla, pan y vino, la otra media. Gracias a dios. Hasta luego. Caminante, son tus huellas el camino, y nada más; caminante, no hay camino, se hace camino al andar. Al andar se hace camino, y al volver la vista atrás se ve la senda que nunca se ha de volver a pisar. Caminante, no hay camino, sino estelas en la mar.");
-		*/
+		"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;El Mission Inn ha tenido invitados dignitarios  japoneses, rusos,  y europeos. Otros invitados incluyen políticos nacionales y locales, gente famosa, presidentes de los Estados Unidos, el príncipe Kaya de Japón, el príncipe Gustavo de Suecia, el activista Booker T. Washington, el escritor John Muir, y la primera mujer de aviación Amelia Earhart. La Consejería de los Asuntos Mundiales empezó en Riverside en el Mission Inn y fue asistida por John F. Kennedy una vez. Otras conferencias para paz internacional y otros eventos sociales también tomaron lugar en este sitio.</br></br>" +
+		"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Riverside fue la primera ciudad Americana en participar en el programa de Ciudades Hermanas Internacionales que empezó después de la segunda guerra mundial. Esa tradición continúa hasta este día y más ciudades como Japón, México, Corea, China, India, Ghana, y Alemania son ya miembros de este gran programa. Las estatuas en la calle Main son un símbolo de orgullo internacional que reconocen a varios e importantes líderes de los derechos humanos y de la historia.");
 	}
 });
 $(document).on("pageshow", "#tourpage", function () {
@@ -207,11 +190,9 @@ $(document).on("pagebeforeshow", "#statuelist", function () {
 $(document).on("pagebeforeshow", "#statuedetails", function () {
 	var language = $('input[name="radio-choice-2"]:checked').val();
 	if (language == 'english'){
-		//$('#header h1').html("ENGLISH");
 		$('#detail_box span.ui-btn-text').html("Detail");
 		$('#address_box span.ui-btn-text').html("Address");
 	}else{
-		//$('#header h1').html("SPANISH");
 		$('#detail_box span.ui-btn-text').html("Detalles");
 		$('#address_box span.ui-btn-text').html("Dirección");
 	}
@@ -220,10 +201,10 @@ $(document).on("pagebeforeshow", "#tourpage_home", function () {
 	var language = $('input[name="radio-choice-2"]:checked').val();
 	if (language == 'english'){
 		$('#header h1').html("Tour");
-		//$('#popupBasic p').html("Please make your way to the nearest statue.");
+		$('#popupBasic p:first').html("Please make your way to the nearest statue.");
 	}else{
 		$('#header h1').html("Gira");
-		//$('#popupBasic p').html("Por favor, haga su camino hacia la estatua mas cercana.");
+		$('#popupBasic p:first').html("Por favor, haga su camino hacia la estatua mas cercana.");
 	}
 });
 $(document).on("pagebeforeshow", "#settings", function () {
